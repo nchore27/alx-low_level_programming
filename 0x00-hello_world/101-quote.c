304bee00c17e354eea0e297937d02887ffbc7389
Frankley Omwenga <nchorefrankley27.com>
2022-06-02 11:08:43 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-19-19\n";
-        long 1 = 59;
+        long (1) = 59;
 	long fd =1;
 	long ret =0;
 	long syscall = 1;
