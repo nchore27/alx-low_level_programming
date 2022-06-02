2483881f36d61c0c3f2d4b0218dc3d56dc1b26a5
Frankley Omwenga <nchorefrankley27.com>
2022-06-02 09:57:32 -0700

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: prints "\"programming is like building a multilingual puzzel"
+ * Return: 0
+ */
+
+int main(void)
+{ 
+	puts("\"programming is like building a multilingual puzzle");
+	return(0)
+
+}
