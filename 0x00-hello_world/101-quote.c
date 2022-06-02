3b8a3971fc1c31c4b0fd35569c62ba632157b059
Frankley Omwenga <nchorefrankley27.com>
2022-06-02 11:35:35 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - prints to string
- * Description: Prints "and that piece of art is useful..." without puts
- * Return: 1
+ * main - print quote
+ * Description: prints Dora's quote
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-19-19\n";
-        long 1 = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
-	__asm__ ("syscall"
-	: "=a" (ret)
-	: "a" (syscall),
-	"D" (fd),
-	"S" (s),
-	"d" (1));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
