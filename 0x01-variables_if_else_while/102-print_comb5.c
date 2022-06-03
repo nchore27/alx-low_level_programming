e1785ae42c4bab3d5f8bb543900da2edf00f3ee4
Frankley Omwenga <nchorefrankley27.com>
2022-06-03 12:52:55 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print pairs of double digit combos
+ *
+ * Description: print pairs of double digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int k, l;
+
+	for (k = 0; k < 100; k++)
+	{
+		for (l = 0; l < 100; l++)
+		{
+			if (k < l)
+			{
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				putchar(' ');
+				putchar((l / 10) + 48);
+				putchar((l % 10) + 48);
+				if (k != 98 || l != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
