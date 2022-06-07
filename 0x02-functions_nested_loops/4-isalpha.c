0baf7f420a24265de477e1611446afce2a1dda55
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 16:01:07 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha- checks if character is uppercase
+ * _isalpha- checks for alphabetic character
  * @c: is a character to be checked
- * Returns: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
