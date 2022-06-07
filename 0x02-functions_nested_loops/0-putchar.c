012d8e54745179203f70b9a3b53d8819eac42a10
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 08:49:39 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main - _putchar
+ * Task - Write a program that prints _putchar, followed by a new line
+ * Return: 0
+ */
+
+int main(){
+	char x[] = "putchar";
+
+	printf("\"_%s\",\n", x);
+	
+	return (0);
+}
