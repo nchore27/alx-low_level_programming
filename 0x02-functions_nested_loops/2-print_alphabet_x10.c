191877e240b62d1b5a648a92cf53c3b92dc2f9c5
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 14:34:59 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int t = 0;
+
+	while (t <= 9)
+	{
+		for(alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+		t++;
+	}
+	return (0);
+}
