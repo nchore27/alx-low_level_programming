ab3c6dec39b6718a8706ef3d14a2dea8135dff03
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 15:47:09 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,15 +5,12 @@
  * @c: is a character to be checked
  * Returns: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if (c < 65 && c> 122)
-	{
-		return (0);
-	}
-	else 
+	if ((c >= 65 || c <= 90) && (c >=97 && c <= 122))
 	{
 		return (1);
 	}
+
+	return (0);
 }
