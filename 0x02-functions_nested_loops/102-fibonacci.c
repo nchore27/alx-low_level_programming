2715889779ad2033781df0ccc7b9ff896c6d72cb
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 18:50:43 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
  /**
-  * main- prints the first 50 Fibonacci numbers
+  * main - prints the first 50 Fibonacci numbers
   * starting with 1 and 2
+  * followed by a new line
   * Return: 0
   */
 int main(void)
