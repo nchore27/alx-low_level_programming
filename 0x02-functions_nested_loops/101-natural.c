ff643c533d4ecdeab0838b112e3efe8a371d5590
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 18:32:25 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-  program that computes and prints the sum of all the multiple
+ * Return: 0
+ */
+
+int main(void)
+{
+	int u, v = 0;
+
+	while (u < 1024)
+	{
+		if ((u % 3 == 0) || (i % 5 == 0))
+		{
+			v += i;
+		}
+		u++;
+	}
+	printf("%d\n", v);
+	returb (0);
+}
