344d2b7675fbf923b35356ac86e56e8cc561a6f8
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 18:57:54 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
  /**
-  * main - prints the first 50 Fibonacci numbers
-  * Description - Write a program that prints the first 50 Fibonacci number
+  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+  * followed by a new line
   * Return: 0
   */
+
 int main(void)
 {
 	long int p, q, r, next;
