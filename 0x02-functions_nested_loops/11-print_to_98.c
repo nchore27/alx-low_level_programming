a50557effe900df4e3cb8205e3e391d6eda3fa62
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 18:00:52 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,22 +3,25 @@
 /**
  * print_to_98- prints all natural numbers from n to 98
  * @n: The number to start printing from
- * Return: 0
+ * Return: void
  */
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
-	}
-
-	else
-	{
 		while (n < 98)
-			printf("%d, ", n++);
-	printf("%d, ", n);
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
-
