8da3d1d3fdfb9f8a9e738c5241b749d0885fb80a
Frankley Omwenga <nchorefrankley27.com>
2022-06-07 17:23:01 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			if (c <= 9)
 				_putchar(' ');
 			else 
-				_putchar((c / 10) + '0')
+				_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
