33d94efcb0ce7a7cf4bb0d909b8b326f9492d568
Frankley Omwenga <nchorefrankley27.com>
2022-06-09 06:07:40 -0700

    c debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,26 +1,8 @@
-positive_or_negative()
-int _putchar(char c);                                                                                   
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet(void);                                                                              
+#include <stdio.h>
 
-void print_alphabet_x10(void);                                                                          
+void positive_or_negative(int i);
 
-int _islower(int c);                                                                                    
-
-int _isalpha(int c);                                                                                    
-
-int print_sign(int n);                                                                                  
-
-int _abs(int);                                                                                          
-
-int print_last_digit(int);                                                                              
-
-void jack_bauer(void);                                                                                  
-
-void times_table(void);                                                                                 
-
-int add(int, int);                                                                                      
-
-void print_to_98(int n);                                                                                
-
-void print_times_table(int n);
+#endif /* MAIN_H */
