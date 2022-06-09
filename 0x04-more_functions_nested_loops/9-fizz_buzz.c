3574a18ae446f6ff1301179499370614584ed21e
Frankley Omwenga <nchorefrankley27.com>
2022-06-09 15:02:38 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,12 +16,16 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 			printf("FizzBuzz");
+		
 		else if ((n % 3) == 0)
 			printf("Fizz");
+		
 		else if ((n % 5) == 0)
 			printf("Buzz");
+		
 		else
 			printf("%d", num);
+		
 		if (n == 100)
 			continue;
 		printf(" ");
