0112ce42ed0c947e4b296c31d26b6c5de23c8d9c
Frankley Omwenga <nchorefrankley27.com>
2022-06-09 10:01:16 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,13 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * followed by a new line
- * _putchar: prints out the standard output
  * Return: void
  */
 
 void print_numbers(void)
 {
 	int i;
-
-	for (i = 0; i <= 9; ++i)
+	for (i = '0'; i <= '9'; ++i)
 	{
 		_putchar(i);
 	}
