3e3b3ddf27acabbe833d1a767fe0afd38c9b118d
Frankley Omwenga <nchorefrankley27.com>
2022-06-09 05:38:50 -0700

    c debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** 
+ * main - file must test that the function positive_or_negative()
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
