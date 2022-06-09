c59ef31b9a8a0b2c2ac10edbf3876526786cf6aa
Frankley Omwenga <nchorefrankley27.com>
2022-06-09 14:14:10 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * followed by a new line
+ * Return: Void.
+ */
+
+void more_numbers(void)
+{
+	int i, j = 10;
+
+	while (j--)
+	{
+		for (i = 0; i <= 14; ++i)
+		{
+			if (i >= 10)
+			
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
