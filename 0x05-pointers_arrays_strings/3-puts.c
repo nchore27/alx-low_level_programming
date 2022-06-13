d36950e02f1a61cdbdea077d219ea8938c044ce6
Frankley Omwenga <nchorefrankley27.com>
2022-06-13 03:30:11 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int f = 0;
+
+	while (str[f] != '\0')
+	{
+		_putchar(str[f]);
+		f++;
+	}
+
+	_putchar('\n');
+}
