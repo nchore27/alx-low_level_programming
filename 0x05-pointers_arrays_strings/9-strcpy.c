e12534fc0f1ffd8af4796b8172c404812d20e656
Frankley Omwenga <nchorefrankley27.com>
2022-06-13 05:12:10 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - function that copies the string pointed to by src
+ * *src: string pointer
+ * *dest: buffer pointer
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int ln = _strlen(src);
+	int x;
+
+	for (x = 0; x < ln; ++x)
+	{
+		dest[x] = src[x]
+	}
+	dest[x] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - function that that prints the length of a string.
+ * @s: string pointer
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while(s[x] != '\0')
+		x++;
+	return (x);
+}
+
