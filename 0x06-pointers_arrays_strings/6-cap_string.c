10954f251f8d1db3d13c0bbf3337650d4ae7283c
Frankley Omwenga <nchorefrankley27.com>
2022-06-16 09:44:13 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,47 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * cap_string - function that capitalizes all words of a string
  * @c - pointer
- * Return: c
+ * Return: always 0
  */
+
 char *cap_string(char *c)
+{
+	int x, y;
+
+	x = 0;
+	char cap[] = ";.!?\"()\t{}\n ";
+	int ln = _strlen(c);
+
+	if (c[x] >= 97 && c[x] <= 122)
+	{
+		c[x] -= 32;
+	}
+	for (x = 0; x < ln; ++x)
+	{
+		for (y = 0; y <  _strlen(cap); ++y)
+		{
+			if (c[x] == cap[y] && c[x + 1] >= 97 && c[x + 1] <= 122)
+			{
+				c[x + 1] -= 32;
+			}
+		}
+	}
+	return (c);
+}
+/**
+ * _strlen - function that that returns the length of a string
+ * @s : pointer to a string.
+ * Return: Length
+ */
+int _strlen(char *s)
 {
 	int x;
 
 	x = 0;
-	if (c[0] >= 'a' && c[0] <= 'z')
-	{
-		c[0] = c[0] - 32;
-	}
-	for (x = 0; c[x] != '\0'; ++x)
-	{
-		switch (c[x])
-		{
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-			case ' ':
-			case '\n':
-			case '\t':
-				if (c[x + 1] > 96 && c[x + 1] < 123)
-				{
-					c[x + 1] = c[x + 1] - 32;
-				}
-		}
-	}
-	return (c);
+	while (s[x] != '\0')
+		++x;
+	return (x);
 }
