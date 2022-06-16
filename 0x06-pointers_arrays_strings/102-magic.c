2bcf87d0d7df49058f20a0cfd17b829d2969a0a8
Frankley Omwenga <nchorefrankley27.com>
2022-06-16 12:14:47 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - 102-magic.c
+ * @a: variable
+ * @p: variable
+ * Return: Always 0
+ */
 
 int main(void)
 {
@@ -9,7 +15,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+	
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
