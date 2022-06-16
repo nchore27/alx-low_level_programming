afb59037ba60c8468fdb1c87a01a96df5d985712
Frankley Omwenga <nchorefrankley27.com>
2022-06-16 10:03:23 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - function that encodes a string into 1337
+ * @n: string
+ * Return: leet
+ */
+
+char *leet(char *n)
+{
+	int x = 0, y;
+	char c1[] = {'a', 'e', 'o', 't', 'l'};
+	char c2[] = {'4', '3', '0', '7', '1'};
+
+	while (n[x] != '\0')
+	{
+		for (y = 0; c1[y] != '\0'; y++)
+		{
+			if (n[x] == c1[y] || n[x] == c1[y] - 32)
+			{
+				n[x] = c2[y];
+			}
+		}
+		++x;
+	}
+	return (n);
+}
