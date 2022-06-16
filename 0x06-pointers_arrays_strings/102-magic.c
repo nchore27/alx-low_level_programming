3c1277211358f9d832b543303e74540a3f881e1a
Frankley Omwenga <nchorefrankley27.com>
2022-06-16 11:06:21 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	int y[5];
+	int *p;
+
+	y[2] = 1024;
+	p = &n;
+
+	/*
+	 * write your line of code here.
+	 * Remember:
+	 * -you are not allowed to use a
+	 * -you are not allowed to modify p
+	 * -only one statement
+	 * -you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("y[2] = %d\n", y[2]);
+	return (0);
+}
