87383b71b4de28e3be9361320d31693c7b623311
Frankley Omwenga <nchorefrankley27.com>
2022-06-16 05:07:03 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @c - pointer
+ * Return: always 0
+ */
+
+char *cap_string(char *c)
+{
+	int x;
+
+	x = 0;
+	if (c[0] >= 'a' && c[0] <= 'z')
+	{
+		c[0] = c[0] - 32;
+	}
+	for (x = 0; c[x] != '\0'; ++x)
+	{
+		switch (c[x])
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+				if (c[x + 1] > 96 && c[x + 1] < 123)
+				{
+					c[x + 1] = c[x + 1] - 32;
+				}
+		}
+	}
+	return (c);
+}
