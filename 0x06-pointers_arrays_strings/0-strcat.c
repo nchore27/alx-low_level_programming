d0dff271e26b1e237b6bb02e8cf2a3b30e55d4d0
Frankley Omwenga <nchorefrankley27.com>
2022-06-16 03:06:04 -0700

    C - More pointers, arrays & strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,11 @@ char *_strcat(char *dest, char *src)
 	int y;
 
 	x = 0;
-	y = 0;
-	
 	while (dest[x] != '\0')
 	{
 		x++;
 	}
+	y = 0;
 	while (src[y] != '\0')
 	{
 		dest[x] = src[y];
@@ -25,6 +24,5 @@ char *_strcat(char *dest, char *src)
 		y++;
 	}
 	dest[x] = '\0';
-	
 	return (dest);
 }
