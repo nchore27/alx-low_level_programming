dc396ab34198bceaf9b4c601481097615f24bd2e
Frankley Omwenga <nchorefrankley27.com>
2022-06-20 04:13:32 -0700

    length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * int _strspn - function that gets the length of a prefix substring
+ * @s: the string
+ * @accept: has bytes from accept
+ * Return: no of bytes from initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+	char fn, fx;
+
+	fx = 0;
+	for (x = 0; *(s + 1) != '\0';)
+	{
+		for (y = 0; *(accept + y) != '\0'; y++)
+		{
+			if (*(s + x) == *(accept + y))
+			{
+				fn = 0;
+				fx = 1;
+				break;
+			}
+			else
+			{
+				fn = 1;
+			}
+		}
+		if (!fn)
+			++x;
+		else
+			break;
+	}
+	return (fx ? x : 0);
+}
