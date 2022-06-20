edfe349c48690f70a105a573d93f81b8d974b3fc
Frankley Omwenga <nchorefrankley27.com>
2022-06-20 11:19:58 -0700

    length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums -  prints the sum of the two diagonals
+ * @a: A pointer.
+ * @size: size of a square matrix
+ * Return: (0)
+ */
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int fn = 0;
+	int fx = 0;
+	int s = size *size;
+
+	for (x = 0; x < s; x += size + 1)
+	{
+		fn += a[x];
+	}
+	for (x = size - 1; x < s - 1; x += size - 1)
+	{
+		fn += a[x];
+	}
+
+	printf("%d, %d\n", fn, fx);
+}
