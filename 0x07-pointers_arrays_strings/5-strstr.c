9e3c82700ce9860468bf1ac9cf7ca916bc725028
Frankley Omwenga <nchorefrankley27.com>
2022-06-20 10:07:59 -0700

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,10 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *l = needle;
 	char *s = haystack;
-	
 	while (*s)
 	{
 		l = needle;
 		s = haystack;
-
 		while (*l)
 		{
 			if (*s == *l)
