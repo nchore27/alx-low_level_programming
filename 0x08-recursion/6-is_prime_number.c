fd5dd2cced9dd52b05aec9d4144b869e02f7c15a
Frankley Omwenga <nchorefrankley27.com>
2022-06-22 10:00:35 -0700

    C - Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _div - checks if int is divisible
+ * @d: the divisor
+ * @i: number to check
+ * Return: 0
+ */
+
+int _div(int i, int d)
+{
+	if (i % d == 0)
+	{
+		return (1);
+	}
+	return (_div(i, d + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: number
+ * Return: 1
+ */
+
+int is_prime_number(int n)
+{
+	int x;
+
+	x = 2;
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	return (_div(n, d));
+}
