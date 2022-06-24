924a907710108b6516dfab6155b33dbd0e891ff1
Frankley Omwenga <nchorefrankley27.com>
2022-06-24 11:25:05 -0700

    prints all arguments it receives

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,24 +4,22 @@
 /**
  * main - A program that multiplies two numbers.
  * @argc: The number of command line arguments.
- * @argv: Array containing the program command line arguments.
+ * @argv: argument vector of pointers to strings.
  * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	int x, y, fon;
+	int x, y, j;
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		puts("Error");
 		return (1);
 	}
-
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
-	fon = x *y;
-
-	printf("%d\n", fon);
+	j = x * c;
+	printf("%d\n", j);
 	return (0);
 }
