725afa47437b8c69a989a6591034a36025f59606
Frankley Omwenga <nchorefrankley27.com>
2022-06-30 16:52:00 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ct;
-	unsigned int len = y, x;
+	unsigned int len = y, index;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (x = 0; s1[x]; x++)
+	for (index = 0; s1[index]; index++)
 		++len;
 
 	ct = malloc(sizeof(char) * (len + 1));
@@ -29,14 +29,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	len = 0;
-	for (x = 0; s1[x]; ++x)
+	for (index = 0; s1[index]; ++index)
 	{
-		ct[++len] = s1[x];
+		ct[++len] = s1[index];
 	}
 
-	for (x = 0; s2[x] && x < y; ++x)
+	for (x = 0; s2[x] && index < y; ++index)
 	{
-		ct[len++] = s2[x];
+		ct[len++] = s2[index];
 	}
 	ct[len] = '\0';
 	return (ct);
