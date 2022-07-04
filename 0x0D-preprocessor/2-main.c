3c6efd95e688ff756175aefe8e628e7ea0ad4eef
Frankley Omwenga <nchorefrankley27.com>
2022-07-04 11:29:52 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file it was compiled from
+ * followed by a new line.
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
