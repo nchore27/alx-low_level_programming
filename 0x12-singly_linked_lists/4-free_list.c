ee773405c86bbae9e2e6a77524c7df65e85ecae1
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-07-15 05:40:32 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: the head node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+	head = NULL;
+}
