ccf56e62dc592e5578bbe0346ae1c473b439dc7c
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-07-18 08:34:03 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_len - function that returns the number of elements in
- * a linked listint_t list.
+ * listint_len - function that returns the number of
+ * elements in a linked listint_t list.
  * @h: This is my structure entry
  * 
  * Return: The number of elements in a linked
@@ -16,6 +16,7 @@ size_t listint_len(const listint_t *h)
 	{
 		return (0);
 	}
+
 	while (h != NULL)
 	{
 		count += 1;
