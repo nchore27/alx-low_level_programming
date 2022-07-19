8e5ba7617a0b8a19092c08e4b99fcd7d2b1bdcab
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-07-19 05:05:11 -0700

    Free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,16 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
-	{
-		return;
-	}
+	listint_t *tmp;
+
 	while (head != NULL)
 	{
-		listint_t *tmp;
-
 		tmp = head;
-		free(tmp);
 		head = head->next;
+		free(tmp);
 	}
 }
