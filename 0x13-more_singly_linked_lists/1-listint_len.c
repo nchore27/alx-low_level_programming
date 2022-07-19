3e5d3d686580577349e7c49805c933cc5fea4f52
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-07-19 05:09:13 -0700

    Free

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,6 @@
 /**
  * listint_len - function that returns the number of elements in a linked listint_t list.
  * @h: This is my structure entry
- * 
  * Return: The number of elements in a linked
  */
 size_t listint_len(const listint_t *h)
